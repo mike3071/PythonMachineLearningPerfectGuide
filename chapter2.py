973ca6f9c490761b56ccdd3164f14403876fce6e
Doyoung Kim <70436657+mike3071@users.noreply.github.com>
2021-01-17 09:04:57 +0900

    Create chapter2.py

---
+++ chapter2.py
@@ -0,0 +1,510 @@
+import sklearn
+print(sklearn.__version__)
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+iris = load_iris()
+ 
+iris_data = iris.data
+ 
+iris_label = iris.target
+print('iris target값:', iris_label)
+print('iris target명:', iris.target_names)
+
+iris_df = pd.DataFrame(data=iris_data, columns=iris.feature_names)
+iris_df['label'] = iris.target
+iris_df.head(3)
+
+X_train, X_test, y_train, y_test = train_test_split(iris_data, iris_label, test_size=0.2, random_state=11)
+ 
+dt_clf = DecisionTreeClassifier(random_state=11)
+
+dt_clf.fit(X_train, y_train)
+
+pred = dt_clf.predict(X_test)
+pred
+
+from sklearn.metrics import accuracy_score
+print('예측 정확도: {0:.4f}'.format(accuracy_score(y_test,pred)))
+
+from sklearn.datasets import load_iris
+
+iris_data = load_iris()
+print(type(iris_data))
+keys = iris_data.keys()
+print('붓꽃 데이터 세트의 키들:', keys)
+print('\n feature_names 의 type:',type(iris_data.feature_names))
+print(' feature_names 의 shape:',len(iris_data.feature_names))
+print(iris_data.feature_names)
+
+print('\n target_names 의 type:',type(iris_data.target_names))
+print(' feature_names 의 shape:',len(iris_data.target_names))
+print(iris_data.target_names)
+
+print('\n data 의 type:',type(iris_data.data))
+print(' data 의 shape:',iris_data.data.shape)
+print(iris_data['data'])
+
+print('\n target 의 type:',type(iris_data.target))
+print(' target 의 shape:',iris_data.target.shape)
+print(iris_data.target)
+
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+iris = load_iris()
+dt_clf = DecisionTreeClassifier()
+train_data = iris.data
+train_label = iris.target
+dt_clf.fit(train_data, train_label)
+
+pred = dt_clf.predict(train_data)
+print('예측 정확도:',accuracy_score(train_label,pred))
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+dt_clf = DecisionTreeClassifier( )
+iris_data = load_iris()
+
+X_train, X_test,y_train, y_test= train_test_split(iris_data.data, iris_data.target, test_size=0.3, random_state=121)
+
+dt_clf.fit(X_train, y_train)
+pred = dt_clf.predict(X_test)
+print('예측 정확도: {0:.4f}'.format(accuracy_score(y_test,pred)))
+
+import pandas as pd
+
+iris_df = pd.DataFrame(iris_data.data, columns=iris_data.feature_names)
+iris_df['target']=iris_data.target
+iris_df.head()
+
+ftr_df = iris_df.iloc[:, :-1]
+tgt_df = iris_df.iloc[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(ftr_df, tgt_df, test_size=0.3, random_state=121)
+                                                    
+print(type(X_train), type(X_test), type(y_train), type(y_test))
+
+dt_clf = DecisionTreeClassifier( )
+dt_clf.fit(X_train, y_train)
+pred = dt_clf.predict(X_test)
+print('예측 정확도: {0:.4f}'.format(accuracy_score(y_test,pred)))
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold
+import numpy as np
+
+iris = load_iris()
+features = iris.data
+label = iris.target
+dt_clf = DecisionTreeClassifier(random_state=156)
+
+
+kfold = KFold(n_splits=5)
+cv_accuracy = []
+print('붓꽃 데이터 세트 크기:',features.shape[0])
+n_iter = 0
+
+
+for train_index, test_index  in kfold.split(features):
+    
+    X_train, X_test = features[train_index], features[test_index]
+    y_train, y_test = label[train_index], label[test_index]
+    
+   
+    dt_clf.fit(X_train , y_train)    
+    pred = dt_clf.predict(X_test)
+    n_iter += 1
+    
+  
+    accuracy = np.round(accuracy_score(y_test,pred), 4)
+    train_size = X_train.shape[0]
+    test_size = X_test.shape[0]
+    print('\n#{0} 교차 검증 정확도 :{1}, 학습 데이터 크기: {2}, 검증 데이터 크기: {3}'
+          .format(n_iter, accuracy, train_size, test_size))
+    print('#{0} 검증 세트 인덱스:{1}'.format(n_iter,test_index))
+    
+    cv_accuracy.append(accuracy)
+    
+
+print('\n## 평균 검증 정확도:', np.mean(cv_accuracy))
+
+import pandas as pd
+
+iris = load_iris()
+
+iris_df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+iris_df['label']=iris.target
+iris_df['label'].value_counts()
+
+kfold = KFold(n_splits=3)
+# kfold.split(X)는 폴드 세트를 3번 반복할 때마다 달라지는 학습/테스트 용 데이터 로우 인덱스 번호 반환. 
+n_iter =0
+for train_index, test_index  in kfold.split(iris_df):
+    n_iter += 1
+    label_train= iris_df['label'].iloc[train_index]
+    label_test= iris_df['label'].iloc[test_index]
+    print('## 교차 검증: {0}'.format(n_iter))
+    print('학습 레이블 데이터 분포:\n', label_train.value_counts())
+    print('검증 레이블 데이터 분포:\n', label_test.value_counts())
+
+from sklearn.model_selection import StratifiedKFold
+
+skf = StratifiedKFold(n_splits=3)
+n_iter=0
+
+for train_index, test_index in skf.split(iris_df, iris_df['label']):
+    n_iter += 1
+    label_train= iris_df['label'].iloc[train_index]
+    label_test= iris_df['label'].iloc[test_index]
+    print('## 교차 검증: {0}'.format(n_iter))
+    print('학습 레이블 데이터 분포:\n', label_train.value_counts())
+    print('검증 레이블 데이터 분포:\n', label_test.value_counts())
+    
+dt_clf = DecisionTreeClassifier(random_state=156)
+
+skfold = StratifiedKFold(n_splits=3)
+n_iter=0
+cv_accuracy=[]
+
+# StratifiedKFold의 split( ) 호출시 반드시 레이블 데이터 셋도 추가 입력 필요  
+for train_index, test_index  in skfold.split(features, label):
+    # split( )으로 반환된 인덱스를 이용하여 학습용, 검증용 테스트 데이터 추출
+    X_train, X_test = features[train_index], features[test_index]
+    y_train, y_test = label[train_index], label[test_index]
+    
+    #학습 및 예측 
+    dt_clf.fit(X_train , y_train)    
+    pred = dt_clf.predict(X_test)
+
+    # 반복 시 마다 정확도 측정 
+    n_iter += 1
+    accuracy = np.round(accuracy_score(y_test,pred), 4)
+    train_size = X_train.shape[0]
+    test_size = X_test.shape[0]
+    
+    print('\n#{0} 교차 검증 정확도 :{1}, 학습 데이터 크기: {2}, 검증 데이터 크기: {3}'
+          .format(n_iter, accuracy, train_size, test_size))
+    print('#{0} 검증 세트 인덱스:{1}'.format(n_iter,test_index))
+    cv_accuracy.append(accuracy)
+    
+# 교차 검증별 정확도 및 평균 정확도 계산 
+print('\n## 교차 검증별 정확도:', np.round(cv_accuracy, 4))
+print('## 평균 검증 정확도:', np.mean(cv_accuracy))
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score , cross_validate
+from sklearn.datasets import load_iris
+import numpy as np
+
+iris_data = load_iris()
+dt_clf = DecisionTreeClassifier(random_state=156)
+
+data = iris_data.data
+label = iris_data.target
+
+# 성능 지표는 정확도(accuracy) , 교차 검증 세트는 3개 
+scores = cross_val_score(dt_clf , data , label , scoring='accuracy',cv=3)
+print('교차 검증별 정확도:',np.round(scores, 4))
+print('평균 검증 정확도:', np.round(np.mean(scores), 4))from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.metrics import accuracy_score
+
+# 데이터를 로딩하고 학습데이타와 테스트 데이터 분리
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris_data.data, iris_data.target, 
+                                                    test_size=0.2, random_state=121)
+dtree = DecisionTreeClassifier()
+
+### parameter 들을 dictionary 형태로 설정
+parameters = {'max_depth':[1, 2, 3], 'min_samples_split':[2,3]}
+
+import pandas as pd
+
+# param_grid의 하이퍼 파라미터들을 3개의 train, test set fold 로 나누어서 테스트 수행 설정.  
+### refit=True 가 default 임. True이면 가장 좋은 파라미터 설정으로 재 학습 시킴.  
+grid_dtree = GridSearchCV(dtree, param_grid=parameters, cv=3, refit=True, return_train_score=True)
+
+# 붓꽃 Train 데이터로 param_grid의 하이퍼 파라미터들을 순차적으로 학습/평가 .
+grid_dtree.fit(X_train, y_train)
+
+# GridSearchCV 결과는 cv_results_ 라는 딕셔너리로 저장됨. 이를 DataFrame으로 변환
+scores_df = pd.DataFrame(grid_dtree.cv_results_)
+scores_df[['params', 'mean_test_score', 'rank_test_score', 
+           'split0_test_score', 'split1_test_score', 'split2_test_score']]
+grid_dtree.cv_results_
+print('GridSearchCV 최적 파라미터:', grid_dtree.best_params_)
+print('GridSearchCV 최고 정확도: {0:.4f}'.format(grid_dtree.best_score_))
+
+# refit=True로 설정된 GridSearchCV 객체가 fit()을 수행 시 학습이 완료된 Estimator를 내포하고 있으므로 predict()를 통해 예측도 가능. 
+pred = grid_dtree.predict(X_test)
+print('테스트 데이터 세트 정확도: {0:.4f}'.format(accuracy_score(y_test,pred)))
+# GridSearchCV의 refit으로 이미 학습이 된 estimator 반환
+estimator = grid_dtree.best_estimator_
+
+# GridSearchCV의 best_estimator_는 이미 최적 하이퍼 파라미터로 학습이 됨
+pred = estimator.predict(X_test)
+print('테스트 데이터 세트 정확도: {0:.4f}'.format(accuracy_score(y_test,pred)))
+from sklearn.preprocessing import LabelEncoder
+
+items=['TV','냉장고','전자렌지','컴퓨터','선풍기','선풍기','믹서','믹서']
+
+# LabelEncoder를 객체로 생성한 후 , fit( ) 과 transform( ) 으로 label 인코딩 수행. 
+encoder = LabelEncoder()
+encoder.fit(items)
+labels = encoder.transform(items)
+print('인코딩 변환값:',labels)
+print('인코딩 클래스:',encoder.classes_)
+print('디코딩 원본 값:',encoder.inverse_transform([4, 5, 2, 0, 1, 1, 3, 3]))
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+
+items=['TV','냉장고','전자렌지','컴퓨터','선풍기','선풍기','믹서','믹서']
+
+# 먼저 숫자값으로 변환을 위해 LabelEncoder로 변환합니다. 
+encoder = LabelEncoder()
+encoder.fit(items)
+labels = encoder.transform(items)
+
+# 2차원 데이터로 변환합니다. 
+labels = labels.reshape(-1,1)
+
+# 원-핫 인코딩을 적용합니다. 
+oh_encoder = OneHotEncoder()
+oh_encoder.fit(labels)
+oh_labels = oh_encoder.transform(labels)
+
+print('원-핫 인코딩 데이터')
+print(oh_labels.toarray())
+print('원-핫 인코딩 데이터 차원')
+print(oh_labels.shape)
+mport pandas as pd
+
+df = pd.DataFrame({'item':['TV','냉장고','전자렌지','컴퓨터','선풍기','선풍기','믹서','믹서'] })
+df
+pd.get_dummies(df)
+from sklearn.datasets import load_iris
+import pandas as pd
+# 붓꽃 데이터 셋을 로딩하고 DataFrame으로 변환합니다. 
+iris = load_iris()
+iris_data = iris.data
+iris_df = pd.DataFrame(data=iris_data, columns=iris.feature_names)
+
+print('feature 들의 평균 값')
+print(iris_df.mean())
+print('\nfeature 들의 분산 값')
+print(iris_df.var())
+from sklearn.preprocessing import StandardScaler
+
+# StandardScaler객체 생성
+scaler = StandardScaler()
+# StandardScaler 로 데이터 셋 변환. fit( ) 과 transform( ) 호출.  
+scaler.fit(iris_df)
+iris_scaled = scaler.transform(iris_df)
+
+#transform( )시 scale 변환된 데이터 셋이 numpy ndarry로 반환되어 이를 DataFrame으로 변환
+iris_df_scaled = pd.DataFrame(data=iris_scaled, columns=iris.feature_names)
+print('feature 들의 평균 값')
+print(iris_df_scaled.mean())
+print('\nfeature 들의 분산 값')
+print(iris_df_scaled.var())
+from sklearn.preprocessing import MinMaxScaler
+
+# MinMaxScaler객체 생성
+scaler = MinMaxScaler()
+# MinMaxScaler 로 데이터 셋 변환. fit() 과 transform() 호출.  
+scaler.fit(iris_df)
+iris_scaled = scaler.transform(iris_df)
+
+# transform()시 scale 변환된 데이터 셋이 numpy ndarry로 반환되어 이를 DataFrame으로 변환
+iris_df_scaled = pd.DataFrame(data=iris_scaled, columns=iris.feature_names)
+print('feature들의 최소 값')
+print(iris_df_scaled.min())
+print('\nfeature들의 최대 값')
+print(iris_df_scaled.max())
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+titanic_df = pd.read_csv('./titanic_train.csv')
+titanic_df.head(3)
+
+print('\n ### train 데이터 정보 ###  \n')
+print(titanic_df.info())
+titanic_df['Age'].fillna(titanic_df['Age'].mean(),inplace=True)
+titanic_df['Cabin'].fillna('N',inplace=True)
+titanic_df['Embarked'].fillna('N',inplace=True)
+
+print('데이터 세트 Null 값 갯수 ',titanic_df.isnull().sum().sum())
+print('데이터 세트 Null 값 갯수 ',titanic_df.isnull().sum())
+
+print(' Sex 값 분포 :\n',titanic_df['Sex'].value_counts())
+print('\n Cabin 값 분포 :\n',titanic_df['Cabin'].value_counts())
+print('\n Embarked 값 분포 :\n',titanic_df['Embarked'].value_counts())
+titanic_df['Cabin'] = titanic_df['Cabin'].str[:1]
+print(titanic_df['Cabin'].head(3))
+titanic_df['Cabin'].value_counts()
+titanic_df.groupby(['Sex','Survived'])['Survived'].count()
+sns.barplot(x='Sex', y = 'Survived', data=titanic_df)
+sns.barplot(x='Pclass', y='Survived', hue='Sex', data=titanic_df)
+# 입력 age에 따라 구분값을 반환하는 함수 설정. DataFrame의 apply lambda식에 사용. 
+def get_category(age):
+    cat = ''
+    if age <= -1: cat = 'Unknown'
+    elif age <= 5: cat = 'Baby'
+    elif age <= 12: cat = 'Child'
+    elif age <= 18: cat = 'Teenager'
+    elif age <= 25: cat = 'Student'
+    elif age <= 35: cat = 'Young Adult'
+    elif age <= 60: cat = 'Adult'
+    else : cat = 'Elderly'
+    
+    return cat
+
+# 막대그래프의 크기 figure를 더 크게 설정 
+plt.figure(figsize=(10,6))
+
+#X축의 값을 순차적으로 표시하기 위한 설정 
+group_names = ['Unknown', 'Baby', 'Child', 'Teenager', 'Student', 'Young Adult', 'Adult', 'Elderly']
+
+# lambda 식에 위에서 생성한 get_category( ) 함수를 반환값으로 지정. 
+# get_category(X)는 입력값으로 'Age' 컬럼값을 받아서 해당하는 cat 반환
+titanic_df['Age_cat'] = titanic_df['Age'].apply(lambda x : get_category(x))
+sns.barplot(x='Age_cat', y = 'Survived', hue='Sex', data=titanic_df, order=group_names)
+titanic_df.drop('Age_cat', axis=1, inplace=True)
+from sklearn import preprocessing
+
+def encode_features(dataDF):
+    features = ['Cabin', 'Sex', 'Embarked']
+    for feature in features:
+        le = preprocessing.LabelEncoder()
+        le = le.fit(dataDF[feature])
+        dataDF[feature] = le.transform(dataDF[feature])
+        
+    return dataDF
+
+titanic_df = encode_features(titanic_df)
+titanic_df.head()
+from sklearn.preprocessing import LabelEncoder
+
+
+def fillna(df):
+    df['Age'].fillna(df['Age'].mean(),inplace=True)
+    df['Cabin'].fillna('N',inplace=True)
+    df['Embarked'].fillna('N',inplace=True)
+    df['Fare'].fillna(0,inplace=True)
+    return df
+
+
+def drop_features(df):
+    df.drop(['PassengerId','Name','Ticket'],axis=1,inplace=True)
+    return df
+
+ 
+def format_features(df):
+    df['Cabin'] = df['Cabin'].str[:1]
+    features = ['Cabin','Sex','Embarked']
+    for feature in features:
+        le = LabelEncoder()
+        le = le.fit(df[feature])
+        df[feature] = le.transform(df[feature])
+    return df
+
+
+def transform_features(df):
+    df = fillna(df)
+    df = drop_features(df)
+    df = format_features(df)
+    return df
+    titanic_df = pd.read_csv('./titanic_train.csv')
+y_titanic_df = titanic_df['Survived']
+X_titanic_df= titanic_df.drop('Survived',axis=1)
+
+X_titanic_df = transform_features(X_titanic_df)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test=train_test_split(X_titanic_df, y_titanic_df, \
+                                                  test_size=0.2, random_state=11)
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+
+dt_clf = DecisionTreeClassifier(random_state=11)
+rf_clf = RandomForestClassifier(random_state=11)
+lr_clf = LogisticRegression()
+
+
+dt_clf.fit(X_train , y_train)
+dt_pred = dt_clf.predict(X_test)
+print('DecisionTreeClassifier 정확도: {0:.4f}'.format(accuracy_score(y_test, dt_pred)))
+
+
+rf_clf.fit(X_train , y_train)
+rf_pred = rf_clf.predict(X_test)
+print('RandomForestClassifier 정확도:{0:.4f}'.format(accuracy_score(y_test, rf_pred)))
+
+
+lr_clf.fit(X_train , y_train)
+lr_pred = lr_clf.predict(X_test)
+print('LogisticRegression 정확도: {0:.4f}'.format(accuracy_score(y_test, lr_pred)))
+
+from sklearn.model_selection import KFold
+
+def exec_kfold(clf, folds=5):
+    
+    kfold = KFold(n_splits=folds)
+    scores = []
+    
+    
+    for iter_count , (train_index, test_index) in enumerate(kfold.split(X_titanic_df)):
+        # X_titanic_df 데이터에서 교차 검증별로 학습과 검증 데이터를 가리키는 index 생성
+        X_train, X_test = X_titanic_df.values[train_index], X_titanic_df.values[test_index]
+        y_train, y_test = y_titanic_df.values[train_index], y_titanic_df.values[test_index]
+        
+         
+        clf.fit(X_train, y_train) 
+        predictions = clf.predict(X_test)
+        accuracy = accuracy_score(y_test, predictions)
+        scores.append(accuracy)
+        print("교차 검증 {0} 정확도: {1:.4f}".format(iter_count, accuracy))     
+    
+     
+    mean_score = np.mean(scores)
+    print("평균 정확도: {0:.4f}".format(mean_score)) 
+# exec_kfold 호출
+exec_kfold(dt_clf , folds=5)
+
+from sklearn.model_selection import cross_val_score
+
+scores = cross_val_score(dt_clf, X_titanic_df , y_titanic_df , cv=5)
+for iter_count,accuracy in enumerate(scores):
+    print("교차 검증 {0} 정확도: {1:.4f}".format(iter_count, accuracy))
+
+print("평균 정확도: {0:.4f}".format(np.mean(scores)))
+from sklearn.model_selection import GridSearchCV
+
+parameters = {'max_depth':[2,3,5,10],
+             'min_samples_split':[2,3,5], 'min_samples_leaf':[1,5,8]}
+
+grid_dclf = GridSearchCV(dt_clf , param_grid=parameters , scoring='accuracy' , cv=5)
+grid_dclf.fit(X_train , y_train)
+
+print('GridSearchCV 최적 하이퍼 파라미터 :',grid_dclf.best_params_)
+print('GridSearchCV 최고 정확도: {0:.4f}'.format(grid_dclf.best_score_))
+best_dclf = grid_dclf.best_estimator_
+
+ 
+dpredictions = best_dclf.predict(X_test)
+accuracy = accuracy_score(y_test , dpredictions)
+print('테스트 세트에서의 DecisionTreeClassifier 정확도 : {0:.4f}'.format(accuracy))
